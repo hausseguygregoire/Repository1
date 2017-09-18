bd2f12e6b7ea64b45b24ae46ee5bdf5b7e4e61b7
Gregoire HAUSSEGUY <hausseguy_gregoire@yahoo.fr>
2017-09-18 15:42:30 +0200

    premier commit

---
+++ serialize.php
@@ -0,0 +1,18 @@
+<?php
+
+$tab['name'] = 'neymar au PSG';
+$tab['id'] = 5;
+var_dump($tab);
+$ser = serialize($tab);
+var_dump($ser);
+// $test = unserialize($ser);
+// var_dump($test);
+$ser_b64 =  base64_encode($ser);
+var_dump($ser_b64);
+echo '<hr>';
+$ser_b64decode =  base64_decode($ser_b64);
+var_dump($ser_b64decode);
+$test = unserialize($ser_b64decode);
+var_dump($test);
+
+echo : ' tout va bien';
