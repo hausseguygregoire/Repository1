385cab3391b6b4d3732e2ad9d3c90dcb98f89e71
Gregoire HAUSSEGUY <hausseguy_gregoire@yahoo.fr>
2017-09-19 11:01:54 +0200

    2e commit 2e commit

---
+++ Form1.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta charset="UTF-8">
+<title> Titre </title>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+
+<body>
+<div class="row">
+	<div class="col-md-10"
+	
+	<!-- formulaire -->
+<form action="merci.html" method="POST">
+	<p>Saisissez votre mail <br>
+	<input name="mail" type="text"></p>
+	
+	<p>Saisissez mot de passe <br>
+	<input name="password" type="text"></p>
+	<p> Commentaires <br>
+	
+	<textarea name="comment" cols="25" rows="5"></textarea> </p>
+	
+<p>
+<input name="choice1" type="checkbox"> choix 1
+<input value="wesh wesh" name="choice2" type="checkbox"> choix 2
+</p>
+	
+<p><input type="submit"></p>	
+	
+</form>
+</body>
+
+</html>
