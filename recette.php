c6096ed23d22618d9d985133049638a687970ece
Gregoire HAUSSEGUY <hausseguy_gregoire@yahoo.fr>
2017-09-19 12:29:49 +0200

    :wq

--- recette.php
+++ recette.php
@@ -1,65 +1,46 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 <meta charset="UTF-8">
-<title>Formulaire - select</title>
+<title> Titre </title>
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 </head>
+
 <body>
 <div class="row">
-<div class="col-md-1">
-</div>
-<div class="col-md-10">
-<!-- form -->
+	<div class="col-md-10">
+	
+	<!-- formulaire -->
 <form action="merci.html" method="GET">
-<!--
-nom
-duree
-type
-saison
--->
-<p>
-<label for="recipe_name">
-Nom de la recette
-</label> :<br>
+	<p>Nom de la recette<br>
+	<input name="recette" type="text"></p>
+	
+	<p>Temps de preparation<br>
+	<input name="temps" type="text"></p>
+ <br>
+	
+	
+<p>	Type de plat </p>
 
-<input id="recipe_name" name="name" type="text"></p>
+<input value="1" name="choice1" type="radio"> entrée
+<input value="2" name="choice1" type="radio"> plat
+<input value="3" name="choice1" type="radio"> dessert
+</p>
 
-<p>Durée de la préparation:<br>
-<input id="duree_preparation" name="time" type="text"></p>
+	<p> Saison conseillée </p><br>
+	<select name="saison">
+		<option value="1">hiver</option>
+		<option value="2">printemps</option>
+		<option value="3">ete</option>
+		<option value="4">automne</option>
 
-<p>Type de plat<br>
-<input id="type_plat_1" name="type_plat" value="1" type="radio">
-<label for="type_plat_1">Entrée</label>
-<input id="type_plat_2" name="type_plat" value="2" type="radio">
-<label for="type_plat_2">Plat</label>
-<input id="type_plat_3" name="type_plat" value="3" type="radio">
-<label for="type_plat_3">Dessert</label>
-</p>
-<p>Durée de préparation :<br>
-<textarea name="preparation" cols="35" rows="5"></textarea>
-</p>
-<p><label for="saison">Quelle saison ?</label>:<br>
-
-<select id="saison" name="saison_id">
-<option value="0">Votre choix :</option>
-<option value="1">été</option>
-<option value="2">automne</option>
-<option value="3">hiver</option>
-<option value="3">printemps</option>
-</select>
-</p>
-<p>
-<input type="submit" value="Valider la recette">
-</p>
+		</select>
+		<br><br>
+<p><input type="submit"></p>	
+	
 </form>
-
-
-
-</div>
-
-
-</div>
 </body>
+
 </html>
